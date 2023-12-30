57b70ebba62b37d2c5ca7346dfdb2a93d907ce2d
Wolf <104569969+0x00wolf@users.noreply.github.com>
2023-12-30 17:14:02 -0400

    Update hkrsai.py

--- hkrsai.py
+++ hkrsai.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from src.args import fetch_args
 from src.pathfinder import PathFinder
 from src.client import Client
@@ -23,7 +24,11 @@ hkrsAI.v2
 def main():
     print(HKRSAI)
     args = fetch_args()  # command-line arguments
-    paths = PathFinder()  # necessary paths for runtime
+
+    paths = PathFinder(cwd=os.path.dirname(os.path.abspath(__file__)))
+    parser = InputParser()  # Class to parse user input and return Actions.
+    dispatcher = Dispatcher()  # Manages conversation state and turnsActions into functions.
+    logger = Logger(paths=paths, log_level=args.log_level, log_format=args.log_format)
 
     client = Client(config=paths.config)  # OpenAI API client management object
     client.initialize()  # Checks for valid saved API key or prompts user. Tests keys before proceeding
@@ -37,9 +42,6 @@ def main():
         presence_penalty=args.presence_penalty,
         max_tokens=args.max_tokens
     )
-    parser = InputParser()  # Class to parse user input and return Actions.
-    dispatcher = Dispatcher()  # Manages conversation state and turnsActions into functions.
-    logger = Logger(paths=paths, log_level=args.log_level, log_format=args.log_format)
 
     system_prompt = SystemPrompt(prompts_dir=paths.prompts, path=args.system_prompt)
     conversation = Conversation().start(system_prompt.content)  # program manages a conversation
