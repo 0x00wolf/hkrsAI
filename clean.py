7abb724083caa79144a91eee4aa4dda6a41e9794
Wolf <104569969+0x00wolf@users.noreply.github.com>
2024-01-03 00:45:43 -0400

    Add files via upload

---
+++ clean.py
@@ -0,0 +1,21 @@
+import sys
+import json
+
+
+if len(sys.argv) != 2:
+    print('error, no file provided')
+else:
+    print(f'attempting to clean {sys.argv[1]}')
+try:
+    with open(sys.argv[1], 'r') as f:
+        data = json.load(f)
+    output_file = f'{sys.argv[1]}.clean.txt'
+    out_file = open(output_file, 'w')
+    for i in range(len(data)):
+        out_file.write(f"----------------\n$ {data[i]['role']}:\n\n" \
+                f"{data[i]['content']}\n\n")
+    out_file.close()
+    print(f'saving cleaned log to: {output_file}')
+except Exception as e:
+    print(f'error {e}')
+    sys.exit()
